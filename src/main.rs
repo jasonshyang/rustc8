700018c30f0d2b5b8cd81119f2f5254f4181e10d
Jason Yang <167345977+jasonshyang@users.noreply.github.com>
2024-12-27 17:23:26 +0000

    minor bug fix

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn run(mut terminal: DefaultTerminal, path: &str) -> io::Result<()> {
     let rom = read_rom(path);
     chip8.load_rom(&rom);
 
-    let cycle_rate = Duration::from_micros(2000);
+    let cycle_rate = Duration::from_micros(1000 / 60);
     let refresh_rate = Duration::from_millis(1000 / 60);
     let mut last_cycle = Instant::now();
     let mut last_refresh = Instant::now();
